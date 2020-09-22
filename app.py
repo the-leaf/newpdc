5dccfd744591dab0617ec4772ea4bf07b11f92e1
ZixSero <tkp2147.abyss@gmail.com>
2020-09-22 14:05:32 +0700

    add files

---
+++ app.py
@@ -0,0 +1,103 @@
+
+import requests
+import traceback
+import pandas as pd
+from bs4 import BeautifulSoup
+from flask import Flask, request, abort
+import sys 
+import os
+sys.path.append(os.path.abspath("/"))
+from sss import *
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+
+app = Flask(__name__)
+
+# Channel Access Token
+line_bot_api = LineBotApi('uKVOAb1Bn3EeGsMBjCdRYkYXqrZFIv/ukCFc/SPBYIRsBKTcR92ktaKyY1JKgqW0k00uIyY0uHscO0rJaxMfS+6QBLv3zjMsPgem76rTCrw1NaHvOvDlPBU6sX43W0utxaP9pYCAQeoJNyhvpwUlSAdB04t89/1O/w1cDnyilFU=')
+# Channel Secret
+handler = WebhookHandler('066c395ef7192ab55179e5595dd47bf7')
+@app.route('/')
+def index():
+    return 'OK'
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+    
+# 處理訊息
+@handler.add(MessageEvent, message=TextMessage)
+def handle_text_message(event):
+    try:
+        text = event.message.text
+        
+        user_profile = None
+        if isinstance(event.source, SourceUser):
+            user_profile = line_bot_api.get_profile(event.source.user_id)
+        
+        if text == '#โปร':
+            if isinstance(event.source, SourceUser):
+                profile = line_bot_api.get_profile(event.source.user_id)
+                line_bot_api.reply_message(
+                    event.reply_token, [
+                        TextSendMessage(text='Display name: ' + profile.display_name),
+                        TextSendMessage(text='Status message: ' + str(profile.status_message))
+                    ]
+                )
+                
+            else:
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text="Bot can't use profile API without user ID"))
+        elif text.startswith('#เลข '):  # broadcast 20190505
+            idnum = text.split(' ')[1]
+            pdcfind(idnum)
+            line_msg = pdcfind(idnum)
+            line_bot_api.reply_message(
+                event.reply_token, [
+                    TextSendMessage(text=line_msg),
+                ]
+            )
+            if user_profile:
+                line_send("{}\n{}".format(user_profile.display_name, line_msg))
+
+        elif text.startswith('#ชื่อ '):  # broadcast 20190505
+            xa = text.split(' ')[1]
+            xb = text.split(' ')[2]
+            xname = xa+' '+xb
+            pdcfindname(xname)
+            line_msg = pdcfindname(xname)
+            line_bot_api.reply_message(
+                event.reply_token, [
+                    TextSendMessage(text=line_msg),
+                ]
+            )
+            if user_profile:
+                line_send("{}\n{}".format(user_profile.display_name, line_msg))
+                
+    except Exception as e:
+        print(e)
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        traceback.print_exception(exc_type, exc_value, exc_traceback)
+
+        
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 1230))
+    app.run(debug=True,host='0.0.0.0', port=port)
---
+++ sss.py
@@ -0,0 +1,143 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+login_url = 'http://pdc.police.go.th/index.php'
+url_out = "http://pdc.police.go.th/arrest_search?KT_logout_now=1"
+get_url = 'http://pdc.police.go.th/smart_c.php'
+get_url3 = 'http://pdc.police.go.th/result_aa.php'
+wan_url=''
+now = datetime.now()
+
+url = 'https://notify-api.line.me/api/notify'
+token = 'cW3MsD9e9Qod9OyrPzTzRCF8MroAl91PnMlRSysxBER'
+headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+token}
+
+
+payload1 = {
+    'hiddenField': '2020-03-15 12:58:59',
+    'username': 'pdc',
+    'password': '1632',
+    'kt_login1': '',
+    'token': ''
+    
+}
+
+payload2 = {
+    'smart': '0' 
+}
+
+payload3 = {
+    'fname' : '0'
+}
+
+def listToString(s):  
+    
+    # initialize an empty string 
+    str1 = "" 
+    
+    # return string   
+    return (str1.join(s)) 
+
+def tableDataText(table):
+        rows = []
+        trs = table.find_all('tr')
+        headerow = [td.get_text(strip=True) for td in trs[0].find_all('th')]
+        if headerow: # if there is a header row include first
+            rows.append(headerow)
+            trs = trs[1:]
+            for tr in trs: # for every table row
+                rows.append([td.get_text(strip=True) for td in tr.find_all('td')]) # data row
+                return rows
+def pdcfind(ss):
+        result = 0
+        i = 0
+        with requests.Session() as session:
+            msx = []
+            payload2['smart'] = ss
+            dt_string = now.strftime("%y-%m-%d %H:%M:%S")
+            payload1['hiddenField'] = dt_string
+            post = session.post(login_url, data=payload1)
+            r = session.post(get_url, data=payload2)
+            r.encoding = 'utf-8'
+            html = r.text
+            soup = BeautifulSoup(html, "html.parser")
+            page = soup.find('p').getText()
+            htmltable = soup.find('table', { 'id' : 'mydata' })
+            if page != "จำนวน 0 หมาย" :
+                for a in htmltable.find_all('a', href=True):
+                    i+=1
+                    wan_url = "http://pdc.police.go.th/"+a['href']
+                    x = session.post(wan_url)
+                    x.encoding = 'utf-8'
+                    htmlwan = x.text
+                    soupwan = BeautifulSoup(htmlwan, "html.parser")
+                    array = []
+                    for b in soupwan.find_all('td'):
+                        array.append(b.text.replace("\n", " ").replace("  ", " ").replace("\xa0", ""))
+                    jj = ("บัตรประชาชน :"+array[1]+"  สถานะ :"+array[19]+"  หมายจับที่ :"+array[9]+"  คดีอาญาที่ :"+array[13]+"  ข้อหา :"+array[11]+"  สภ.เจ้าของหมาย :"+array[23])
+                    msx.append(jj)
+                list_table = tableDataText(htmltable)
+                msg_line1 = "ชื่อ : "+list_table[1][2]+"  พบหมายจับ "+str(i)+" หมาย"+"\n"
+                line_msg = []
+                for k in range(i):
+                    line_msg.append("("+str(k+1)+")"+msx[k]+"\n")
+                msg_line2 = listToString(line_msg)
+                line_send = msg_line1+msg_line2
+                logout = session.post(url_out)
+                return line_send
+            else:
+                line_send = "ไม่พบหมายจับ"
+                logout = session.post(url_out)
+                return line_send        
+        #r = requests.post(url, headers=headers , data = {'message':line_send})
+
+def pdcfindname(sn):
+        result = 0
+        i = 0
+        with requests.Session() as session:
+            msx = []
+            payload3['fname'] = sn
+            dt_string = now.strftime("%y-%m-%d %H:%M:%S")
+            payload1['hiddenField'] = dt_string
+            post = session.post(login_url, data=payload1)
+            r = session.post(get_url3, data=payload3)
+            r.encoding = 'utf-8'
+            html = r.text
+            soup = BeautifulSoup(html, "html.parser")
+            page = soup.find('p').getText()
+            htmltable = soup.find('table', { 'id' : 'mydata' })
+            if page != "พบ 0 หมาย" :
+                for a in htmltable.find_all('a', href=True):
+                    i+=1
+                    wan_url = "http://pdc.police.go.th/"+a['href']
+                    x = session.post(wan_url)
+                    x.encoding = 'utf-8'
+                    htmlwan = x.text
+                    soupwan = BeautifulSoup(htmlwan, "html.parser")
+                    array = []
+                    for b in soupwan.find_all('td'):
+                        array.append(b.text.replace("\n", " ").replace("  ", " ").replace("\xa0", ""))
+                    jj = ("บัตรประชาชน :"+array[1]+"  สถานะ :"+array[19]+"  หมายจับที่ :"+array[9]+"  คดีอาญาที่ :"+array[13]+"  ข้อหา :"+array[11]+"  สภ.เจ้าของหมาย :"+array[23])
+                    msx.append(jj)
+                list_table = tableDataText(htmltable)
+                msg_line1 = "ชื่อ : "+list_table[1][3]+"  พบหมายจับ "+str(i)+" หมาย"+"\n"
+                line_msg = []
+                for k in range(i):
+                    line_msg.append("("+str(k+1)+")"+msx[k]+"\n")
+                msg_line2 = listToString(line_msg)
+                line_send = msg_line1+msg_line2
+                logout = session.post(url_out)
+                return line_send
+            else:
+                line_send = "ไม่พบหมายจับ"
+                logout = session.post(url_out)
+                return line_send        
+        #r = requests.post(url, headers=headers , data = {'message':line_send})
+
+
+
+def line_send(message):
+    r = requests.post(url, headers=headers, data={'message': message})
+    print(r.text)
